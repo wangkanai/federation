564c4fed963e5ed573a7560cd7d277515579a6dc
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-08-30 20:17:23 +0700

    feat: update project references and add assertion helper for service validation

---
+++ Federation/tests/Federation/Extensions/AssertionHelperExtensions.cs
@@ -0,0 +1,18 @@
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved.
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Wangkanai.Federation.Extensions;
+
+public static class AssertionHelperExtensions
+{
+   public static void AssertServices(this List<ServiceDescriptor> descriptors, IServiceCollection services)
+   {
+      for (var i = 0; i < descriptors.Count; i++)
+      {
+         Assert.Equal(descriptors[i].ServiceType,            services[i].ServiceType);
+         Assert.Equal(descriptors[i].ImplementationInstance, services[i].ImplementationInstance);
+         Assert.Equal(descriptors[i].Lifetime,               services[i].Lifetime);
+      }
+   }
+}
